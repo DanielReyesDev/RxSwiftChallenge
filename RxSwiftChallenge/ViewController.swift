b258a2acc888f5cb04ab3ef350949b6c71673108
Daniel Reyes Sanchez <daniel.reyes@wizeline.com>
2020-07-16 13:05:25 -0500

    Project creation

---
+++ RxSwiftChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftChallenge
+//
+//  Created by Daniel Reyes Sanchez on 16/07/20.
+//  Copyright © 2020 Walmart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
